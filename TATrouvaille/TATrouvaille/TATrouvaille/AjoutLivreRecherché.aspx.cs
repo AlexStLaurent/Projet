09897ea5674bbabd4131d331563500b13ea00c50
Roflinwaffle <43412393+Roflinwaffle@users.noreply.github.com>
2018-12-18 10:09:58 -0500

    a

--- TATrouvaille/TATrouvaille/TATrouvaille/AjoutLivreRecherché.aspx.cs
+++ TATrouvaille/TATrouvaille/TATrouvaille/AjoutLivreRecherché.aspx.cs
@@ -23,7 +23,7 @@ namespace TATrouvaille
             titre = TxtTitreRecherche.Text; //Assigne le texte de la textbox txtTitreRecherche à la variable titre
             auteur = TxtAuteurRecherche.Text; //de même avec la variable auteur
             SqlConnection con = new SqlConnection(@"Data Source=.; Initial Catalog = TATrouvaille; User ID=sa;Password=sql"); // crée une nouvelle connexion vers la base de donnée (implémenter sur le serveur local)
-            SqlCommand cmd = new SqlCommand($"INSERT INTO LivreRechercher VALUES ('{titre}', '{auteur}')"); //crée une commande SQL pour insérer un livre dans la base de donné
+            SqlCommand cmd = new SqlCommand($"INSERT INTO LivreRechercher VALUES ('{titre}', '{auteur}')", con); //crée une commande SQL pour insérer un livre dans la base de donné
             cmd.Connection.Open(); //ouvre la connexion
             cmd.ExecuteNonQuery(); //execute la commande NonQuery
             ScriptManager.RegisterClientScriptBlock(this, GetType(), "AlertBox", "Alert('Livre ajouté à la liste avec succès')", true); 
